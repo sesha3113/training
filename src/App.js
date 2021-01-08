8add11797495983ec487892432cd7eb6188b4b99
Sesha Prasan Kumaran T R <42384167+sesha3113@users.noreply.github.com>
2021-01-08 13:40:04 +0530

    To add TextField component and changes applied.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
-import Helloworld from "./Pages/helloworld/helloworld";
-import Routes from "./Routes/index";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-const Routing = () => {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Helloworld />
-        
-      </Route>
-    </Switch>
-  );
-};
+import Helloworld from "./Pages/helloworld/index";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
+        <Helloworld />
       </header>
     </div>
   );
