4bf23d12cc0bd43c44e0c6db3de410c77ecfe89d
Sesha Prasan Kumaran T R <42384167+sesha3113@users.noreply.github.com>
2021-01-11 12:46:59 +0530

    Task: 2, Changes applied.

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -7,7 +7,7 @@ function Helloworld() {
   const { TextFieldlabels } = Config;
   return (
     <div>
-      <h1>Login</h1>
+      <h1>Login!!</h1>
       <TextField TextFieldLabel={TextFieldlabels[0]} />
       <Password TextFieldLabel={TextFieldlabels[1]} />
     </div>
